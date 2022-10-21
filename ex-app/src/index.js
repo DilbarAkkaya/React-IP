b80d00906632d22129995fef5a9f80e2147c5558
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-21 23:45:34 +0300

    feat: add class for h2

--- ex-app/src/index.js
+++ ex-app/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const text = 'Hello world'
 const elem = (
   <div>
-    <h2>
-      hello world
+    <h2 className='text'>
+      Text: {text}
     </h2>
     <input type="text" />
-    <button />
+    <button htmlFor='0'>Click me</button>
   </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-elem, root
+elem
 );
