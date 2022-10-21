e84bc6a9602405d5574b14932461a28074638c00
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-21 23:48:38 +0300

    feat: delete elem

--- ex-app/src/index.js
+++ ex-app/src/index.js
@@ -3,18 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const text = 'Hello world'
-const elem = (
-  <div>
-    <h2 className='text'>
-      Text: {text}
-    </h2>
-    <input type="text" />
-    <button htmlFor='0'>Click me</button>
-  </div>
-)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-elem
-);
+root.render(<App/>);
