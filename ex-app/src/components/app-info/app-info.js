7ae9ed8bc3196c68c95ad682d6e0f8c4d6de0eb3
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-22 23:32:50 +0300

    feat: create component app-info, add styles for app-info, import to app.js

---
+++ ex-app/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Accounting of employees in the company N</h1>
+      <h2>Total count of employees:</h2>
+      <h2>Will receive the bonus:</h2>
+    </div>
+  )
+}
+
+export default AppInfo;
