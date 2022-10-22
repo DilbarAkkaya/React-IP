11976e8a3ec0f344b98e65a3e3337449feabd612
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-23 00:07:24 +0300

    feat: create component app-filter, add styles, import to app

--- ex-app/src/components/app/app.js
+++ ex-app/src/components/app/app.js
@@ -1,5 +1,6 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
 import './app.css';
 
 function App () {
@@ -8,6 +9,7 @@ function App () {
       <AppInfo />
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
       </div>
     </div>
   )
