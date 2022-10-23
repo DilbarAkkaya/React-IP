cc9c7ff4a1ec1cfcf13e5c842265b8fbb17e2809
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-23 22:37:26 +0300

    feat: add props for employee list item

--- ex-app/src/components/employee-list-item/employee-list-item.js
+++ ex-app/src/components/employee-list-item/employee-list-item.js
@@ -1,10 +1,10 @@
 import './employee-list-item.css';
 
-const EmployeeListItem = () => {
+const EmployeeListItem = (props) => {
   return (
     <li className="list-group-item d-flex justify-content-between">
-    <span className="list-group-item-label">John Smith</span>
-    <input type="text" className="list-group-item-input" defaultValue="1000$"/>
+    <span className="list-group-item-label">{props.name}</span>
+    <input type="text" className="list-group-item-input" defaultValue={props.salary + '$'}/>
     <div className="d-flex justify-content-center align-items-center">
       <button className="btn-cookie btn-sm" type="button">
         <i className="fas fa-cookie"></i>
--- ex-app/src/components/employee-list/emploee-list.js
+++ ex-app/src/components/employee-list/emploee-list.js
@@ -4,9 +4,9 @@ import './employee-list.css';
 const EmployeeList = () => {
   return (
     <ul className="app-list list-group">
-      <EmployeeListItem/>
-      <EmployeeListItem/>
-      <EmployeeListItem/>
+      <EmployeeListItem name='John C.' salary={800}/>
+      <EmployeeListItem name='Alex M.' salary={3000}/>
+      <EmployeeListItem name='Sara K.' salary={5000}/>
     </ul>
   )
 }
