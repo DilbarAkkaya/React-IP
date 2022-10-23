9b7ff860e598b29a33f2d8bdeec920926747fbba
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-23 23:37:44 +0300

    feat: add key atrib for list

--- ex-app/src/components/employee-list/emploee-list.js
+++ ex-app/src/components/employee-list/emploee-list.js
@@ -3,10 +3,12 @@ import './employee-list.css';
 
 const EmployeeList = ({ data }) => {
   const elements = data.map(item => {
+  const {id, ...itemProps} = item;
     return (
-      <EmployeeListItem {...item}/>
+      <EmployeeListItem key={id}{...itemProps}/>
   )
   })
+
   return (
     <ul className="app-list list-group">
       {elements}
